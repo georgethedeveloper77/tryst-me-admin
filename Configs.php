482ff25ad212cc6373adc6390e5bc37230fe115e
georgethedeveloper77 <georgekarani77@gmail.com>
2022-09-16 00:16:06 +0300

    icon

--- Configs.php
+++ Configs.php
@@ -9,7 +9,11 @@ error_reporting(null);
 session_start();
 
 try {
-    ParseClient::initialize('ASNNDrrmNIHFyedPSwEgWiKYzQWnT6J6JRUA98pL', 'wQp4biPrV8DByDYHWzMichuNenrFEiXmZyGeCwrT', 'dKpMVT8xi0TeCLFSriAo6taqZ5QqhAXcghPuSzxH');
+    ParseClient::initialize(
+        'ASNNDrrmNIHFyedPSwEgWiKYzQWnT6J6JRUA98pL',
+        'wQp4biPrV8DByDYHWzMichuNenrFEiXmZyGeCwrT',
+        'dKpMVT8xi0TeCLFSriAo6taqZ5QqhAXcghPuSzxH'
+    );
     ParseClient::setServerURL('https://parseapi.back4app.com/', '/');
     ParseClient::setStorage(new ParseSessionStorage());
 } catch (Exception $e) {
@@ -20,4 +24,4 @@ if ($health['status'] !== 200) {
 }
 
 // Website root url
-$GLOBALS['WEBSITE_PATH'] = 'https: //tryst-me-admin.herokuapp.com';
+$GLOBALS['WEBSITE_PATH'] = 'https://tryst-me-admin.herokuapp.com';
