4fbd653b7dc967514b2aa05aeedc3c267fc36ec2
georgethedeveloper77 <georgekarani77@gmail.com>
2022-09-16 00:23:32 +0300

    icon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'Configs.php';
 
 use Parse\ParseUser;
 
-// Require https, remove of comment if you are in insecure HTTP or in localhost
+/// Require https, remove of comment if you are in insecure HTTP or in localhost
 /*if ($_SERVER['HTTPS'] !== "on") {
     $url = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
     header("Location: $url"); //php -S localhost:63342
